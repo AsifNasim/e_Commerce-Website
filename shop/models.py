af7513719cc14b37547ddbcabaae4261541754fe
AsifNasim <asifnasimofficial@gmail.com>
2019-08-29 17:52:00 +0530

    DB updated and UI improved

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class product(models.Model):
+    product_ID = models.AutoField
+    product_name = models.CharField(max_length = 50)
+    category = models.CharField(max_length = 50, default ="" )
+    sub_category = models.CharField(max_length = 50, default ="")
+    price = models.IntegerField(default = 0)
+    desc = models.CharField(max_length = 200)
+    pub_date = models.DateField()
+    image = models.ImageField(upload_to = "shop/images", default ="")
+
+
+    def __str__(self):
+        return self.product_name
